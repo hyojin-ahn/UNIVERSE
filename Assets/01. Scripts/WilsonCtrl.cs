40a25b2e5204870a05d7a77dcf3af10f9155ae01
teto0525 <86487948+teto0525@users.noreply.github.com>
2021-08-23 17:36:14 +0900

    pet

--- Assets/01. Scripts/WilsonCtrl.cs
+++ Assets/01. Scripts/WilsonCtrl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,99 +6,113 @@ using UnityEngine;
 public class WilsonCtrl : MonoBehaviour
 {
 	/* 움직임 */
-	Vector3 rot = Vector3.zero;
+	// Vector3 rot = Vector3.zero;
 
 	/* 스피드 */
 	[Range(5.0f, 50.0f)]
-	public float moveSpeed = 20f;
+	public float moveSpeed = 5f;
 	[Range(5.0f, 50.0f)]
-	public float rotSpeed = 20f;
+	public float rotSpeed = 5f;
 
 	/* 애니메이션 */
 	Animator anim;
 
+	public enum State
+    {
+		IDLE,
+		WALK,
+		AttACK
+    }
+	public State pstate = State.IDLE;
+
 	/* 목표물 = 주인공 */
 	public GameObject Player;
-	public Transform pet;
-	public Transform PlayerPos;
 	bool PlayerMove;
 
-	/* 공격 Effect */
-	public GameObject attckEf;
-	bool IsAttack;
-
 	/* 슬롯 UI */
-	public GameObject slot;
+	//생략 public GameObject slot;
 
 	// Use this for initialization
 	void Start()
 	{
 		anim = gameObject.GetComponent<Animator>();
-		gameObject.transform.eulerAngles = rot;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		Vector3 dir = Player.transform.position - this.transform.position;
-		//pet.position = Vector3.Lerp(dir.normalized * 5 * Time.deltaTime);
-		//pet.rotation = Quaternion.Lerp(Vector3.forward * moveSpeed * Time.deltaTime);
-
-		RoboMove();
-		gameObject.transform.eulerAngles = rot;
-	}
-
-	void RoboMove()
-	{
-		/* 걷기 */
-		/* 플레이어와 일정거리 유지하면서 따라오고 플레이어가 돌면 같이 돈다 */
-		if (PlayerMove == true)
-		{
-			anim.SetBool("Walk_Anim", true);
-		}
-		else if (PlayerMove == false)
-		{
-			anim.SetBool("Walk_Anim", false);
-		}
-		/* 만약 플레이어 포스라면 */
-		
-
-		/* 공격 */
-		/* Q 누르면 몸 굴려서 적 공격, 다시 되돌아온다 */  //Enemy -> Pet 태그와 부딪히면 튕겨나가 hp 깎인다
-		/* Q 누르고 있는 동안은 공격 */
-		/* 떼면 원상태로 돌아와서 추적모드 */
-		if (Input.GetKey(KeyCode.Q))
-		{
-			IsAttack = true;
-			EnemyAttack();
-		}
-	}
-
-	/* 애너미 공격하기 */
-	void EnemyAttack()
-    {
-		if (IsAttack == true)
+		switch (pstate)
         {
-			anim.SetBool("Roll_Anim", true);
+			// 만약 플레이어가 idle 이라면 idel 상태로
+			case State.IDLE:
+				idle();
+				break;
+
+			// 만약 플레이어가 움직이면 walk 상태로
+			case State.WALK:
+				walk();
+				break;
+
+			// 만약 공격 버튼 눌려지면 attack 상태로
+			case State.AttACK:
+				attack();
+				break;
+
+        }
+	}
+
+    private void idle()
+    {
+		// 만약 플레이어가 움직이지 않는다면
+		if (PlayerMove == false)
+		{
+			anim.SetBool("IsMove", false);
 		}
+	}
+
+	// 플레이어가 움직이면 플레이어쪽으로 이동하고 싶다
+    private void walk()
+    {
+        if(PlayerMove == true)
+        {
+			anim.SetBool("IsMove", true);
+
+			// PlayerPos 방향으로 이동
+			Vector3 dir = Player.transform.position - transform.position;
+			dir.y = 0;
+			transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotSpeed * Time.deltaTime);
+
+        }
     }
 
-	/* 슬롯 */
-	/* 슬롯 창 세개 제공 */
-	public void Slot()
-    {
-		/* 만약 F2키를 누르면 */
-		if (Input.GetKeyDown(KeyCode.F2))
-        {
-			/* 슬롯 UI가 등장한다 */
-			slot.SetActive(true);
+	/* 공격 Effect */
+	public GameObject attackEf;
+	bool IsAttack;
 
-			if (Input.GetKeyDown(KeyCode.F2))
-			{
-				/* 슬롯 창이 닫힌다 */
-				slot.SetActive(false);
-            }
+    private void attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+			IsAttack = true;
+
+			// roll 준비상태 돌입하기
+			anim.SetBool("IsAttack", true);
+			roll();
         }
+    }
+	/* 애너미 공격하기 */
+	void roll()
+    {
+        // attack()와 roll() 사이에 딜레이가 있었으면 좋겠다 --> 코루틴
+        // Q 버튼 계속해서 누르면 구른다
+        if (Input.GetKey(KeyCode.Q))
+        {
+			anim.SetBool("IsRoll", true);
+			Instantiate(attackEf);
+
+        }
+
     }
 
 }
