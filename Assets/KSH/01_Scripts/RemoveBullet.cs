827e55f131bbc61191fdbc947fa0909ff17c3e47
teto0525 <86487948+teto0525@users.noreply.github.com>
2021-08-26 17:25:26 +0900

    FPS 버그 수정

--- Assets/KSH/01_Scripts/RemoveBullet.cs
+++ Assets/KSH/01_Scripts/RemoveBullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RemoveBullet : MonoBehaviour
 {
     public GameObject sparkEffect;
+    public GameObject groundEffect;
 
     // 충돌 콜백함수(Call Back Function), 이벤트 함수(Event Function)
     void OnCollisionEnter(Collision coll)
@@ -22,6 +23,21 @@ public class RemoveBullet : MonoBehaviour
             Quaternion rot = Quaternion.LookRotation(normal);
             GameObject spark = Instantiate(sparkEffect, pos, rot);
             Destroy(spark, 0.5f);
+        }
+
+        if(coll.collider.tag == "GROUND")
+        {
+            // 총알을 삭제
+            Destroy(coll.gameObject);
+
+            // 스파크 이펙트 , 위치(충돌위치), 법선 벡터
+            Vector3 pos = coll.GetContact(0).point;
+            Vector3 normal = -coll.GetContact(0).normal;
+
+            // 벡터가 바라보는 방향의 각도를 산출
+            Quaternion rot = Quaternion.LookRotation(normal);
+            GameObject sandEF = Instantiate(sparkEffect, pos, rot);
+            Destroy(sandEF, 0.5f);
         }
     }
 }
